ef42d467c1be7ae09d4d808e078f3f39dfd09472
Babakinha <sangueotario@gmail.com>
2021-11-15 22:30:19 -0300

    Proper Lobby

--- client/pages/room.tsx
+++ client/pages/room.tsx
@@ -1,16 +1,27 @@
 import type { NextPage } from "next";
-import { useRouter } from "next/dist/client/router";
-import TestRoom from "../components/Room/TestRoom";
+import React, { useRouter } from "next/dist/client/router";
+import { useEffect } from "react";
+import Lobby from "../components/Lobby/Lobby";
 
 const Room: NextPage = () => {
 	const router = useRouter()
 	const { code, name } = router.query
 	//if (!code || !name) router.back()
 
+	//Hehe kahoot trap music
+	const audio = new Audio("https://r2---sn-bg07dnks.googlevideo.com/videoplayback?expire=1637035930&ei=OtuSYey4Ety0lu8Pq9qKqAY&ip=207.204.248.201&id=o-AHhXEufY6dL23azLRt5wM6aXyeSbmI5365-k7OneGchC&itag=18&source=youtube&requiressl=yes&vprv=1&mime=video%2Fmp4&ns=Zxjz4J-6xXsFNJBhXTMSmEcG&gir=yes&clen=2430190&ratebypass=yes&dur=96.037&lmt=1480483883645184&fexp=24001373,24007246&c=WEB&n=ADYkf3IuS_t4GY-w7q&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cvprv%2Cmime%2Cns%2Cgir%2Cclen%2Cratebypass%2Cdur%2Clmt&sig=AOq0QJ8wRAIgdMrtjeu-OT1sdXZtdBfOOv53ThIL0vRs0S8YTtiZ0s4CIGXk7_ZDflvAyXwU3Gn2c1cX0xhUc1QE2IfbeIqXKsmw&rm=sn-42a4pcxgoxu-hp5e7l,sn-q4fell7l&req_id=5921c65bf302a3ee&ipbypass=yes&redirect_counter=3&cm2rm=sn-gpvey7l&cms_redirect=yes&mh=WU&mip=177.37.250.6&mm=34&mn=sn-bg07dnks&ms=ltu&mt=1637013878&mv=m&mvi=2&pl=23&lsparams=ipbypass,mh,mip,mm,mn,ms,mv,mvi,pl&lsig=AG3C_xAwRgIhANFZc9QG1nd4FXxFW9D2Hzy6LONAWtjcITuII9x974p0AiEAimnsaLxiyqTgky2BMTTrMstyECH1RqijMW0h6D2z_5Q%3D")
+	useEffect(() => {
+		audio.play()
+		audio.loop = true
+		return () => {
+			audio.pause()
+		}
+	}, [])
+
 	return (
 		<div>
 			<h1>Welcome {name}! To room {code}</h1>
-			<TestRoom></TestRoom>
+			<Lobby code={code as string} name={name as string}></Lobby>
 		</div>
 	);
 };
--- server/src/socket.ts
+++ server/src/socket.ts
@@ -1,5 +1,5 @@
 import { Server, Socket } from "socket.io";
-import * as Users from "./users"
+import * as Rooms from "./rooms"
 
 function socket({io}: { io: Server}) {
     console.log(`Sockets enabled`);
@@ -7,33 +7,55 @@ function socket({io}: { io: Server}) {
     io.on("connection", (socket: Socket) => {
         console.log(`User connected ${socket.id}`);
 
-        socket.on('join', ({ name, room }, callback) => {
-            const { error, user } = Users.addUser({id: socket.id, name, room})
-            if(error) {if(callback) {return callback(error)}else return};
+        socket.on('create', ({ name, code }, callback) => {
+            const user = Rooms.createRoom(code, {id: socket.id, name})
+            if((user as { error: string}).error) {if(callback) {return callback((user as { error: string}).error)}else return};
 
-            socket.join(user!.room);
-            socket.broadcast.to(room).emit('join', { name })
+            socket.join(code);
             
-            const players = Users.getUsersInRoom(room);
-            players.forEach(player => {
-                socket.emit('join', {name: player.name })
-            })
+            const players = Rooms.getRoom(code)!.users;
+            socket.emit('roomData', {users: players});
+        })
+
+        socket.on('join', ({ name, code }, callback) => {
+            const user = Rooms.addUser(code, {id: socket.id, name})
+            if((user as { error: string}).error) {if(callback) {return callback((user as { error: string}).error)}else return};
+
+            socket.join(code);
+            
+            const players = Rooms.getRoom(code)!.users;
+            socket.emit('roomData', {users: players})
+            socket.broadcast.to(code).emit('roomData', {users: players})
         })
 
         socket.on('canvas', (canvas, callback) => {
             try {
-                const user = Users.getUser(socket.id);
-                console.log(user!.name + " Sent a canvas")
-                socket.broadcast.to(user!.room).emit('canvas', canvas)
+                const room = Rooms.getUserRoom(socket.id)
+                if(!room) throw 'Room doesn\'t exist cant';
+                socket.broadcast.to(room.code).emit('canvas', canvas)
+            }catch(e) {
+                if(callback) callback(e)
+            }
+        })
+
+        socket.on('changeMode', (mode, callback) => {
+            try {
+                const room = Rooms.getUserRoom(socket.id)
+                if(!room) throw 'Room doesn\'t exist cant';
+                if(room.owner.id != socket.id) throw 'User isn\'t the owner';
+                room.mode = mode
+                socket.broadcast.to(room.code).emit('roomData', {mode})
             }catch(e) {
                 if(callback) callback(e)
             }
         })
 
         socket.on('disconnect', () => {
-            const user = Users.getUser(socket.id)
-            if (user?.room) socket.broadcast.to(user.room).emit('leave', { name: user.name})
-            Users.removeUser(socket.id)
+            const room = Rooms.getUserRoom(socket.id)
+            if(!room) return;
+
+            Rooms.removeUser(room, socket.id)
+            socket.broadcast.to(room.code).emit('roomData', {users: room.users})
             console.log(`User Disconnected ${socket.id}`);
         })
     })
